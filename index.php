b64dedabd8498c85ca557e51e4853af2edee1407
Jake Buller <jake.buller@enflick.com>
2016-04-26 23:10:27 -0400

    remove partners from home page

--- index.php
+++ index.php
@@ -32,26 +32,6 @@ require_once("header.php");
 		</div>
 	</div>        
 </div>
-    
-		<div class="header">
-			<div class="content">
-				<h1>Proud members of and partners with:</h1>
-			</div>
-		</div>		
-	<div class="content">
-        <table id="partner-table">
-            <tr>
-                <td><a href="#"><img src="images/toshiba.png"</a></td>
-                <td><a href="#"><img src="images/sensopart.png"</a></td>
-                <td rowspan="2"><a href="#"><img src="images/easa.png"</a></td>
-                <td rowspan="2"><a href="#"><img src="images/ecra.png"</a></td>
-            </tr>
-            <tr>
-                <td><a href="#"><img src="images/euchner.png"</a></td>
-                <td><a href="#"><img src="images/reer.png"</a></td>
-            </tr>
-        </table>
-    </div>
       
 <?php
 require_once("footer.php");
