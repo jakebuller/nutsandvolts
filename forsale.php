7de86e8702049465bbf57a8c5de4017d5f226cb8
Jake Buller <jake.buller@enflick.com>
2018-09-13 09:07:12 -0400

    Update forsale.php

--- forsale.php
+++ forsale.php
@@ -11,27 +11,9 @@ require_once("header.php");
         If you're interested in purchasing this item please contact me by clicking <a href="mailto:mike@nutsandvolts.ca">here</a>
     </div>
     <div class="content" id="for-sale-image-container">
-        <div>
-            <img src="images/forsale/IMG_20160627_141552.jpg">
-        </div>
-        <div>
-            <img src="images/forsale/IMG_20160627_141601.jpg">
-        </div>
-        <div>
-            <img src="images/forsale/IMG_20160627_141612.jpg">
-        </div>
-        <div>
-            <img src="images/forsale/IMG_20160627_141751.jpg">
-        </div>
-        <div>
-            <img src="images/forsale/IMG_20170407_133819.jpg">
-        </div>
-        <div>
-            <img src="images/forsale/IMG_20170407_133843.jpg">
-        </div>
-        <div>
+<!--         <div>
             <img src="images/forsale/IMG_20170407_133944.jpg">
-        </div>
+        </div> -->
     </div>
 </div>
 
