762c7734515df4d698f234c926d2064d8c04d1dc
Jake Buller <jake.buller@enflick.com>
2018-09-13 09:12:39 -0400

    Update forsale.php

--- forsale.php
+++ forsale.php
@@ -7,14 +7,14 @@ require_once("header.php");
 </div>
 
 <div id="for-sale-wrapper">
-    <div class="centered-text">
+<!--     <div class="centered-text">
         If you're interested in purchasing this item please contact me by clicking <a href="mailto:mike@nutsandvolts.ca">here</a>
     </div>
     <div class="content" id="for-sale-image-container">
-<!--         <div>
+        <div>
             <img src="images/forsale/IMG_20170407_133944.jpg">
-        </div> -->
-    </div>
+        </div>
+    </div> -->
 </div>
 
 <?php
